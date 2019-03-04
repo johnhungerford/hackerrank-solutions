2632ba1e49f9414c4745c5f19f5e1154cfcb1f98
johnhungerford <hungerfordjustice@gmail.com>
2019-03-04 11:27:15 -0500

    Interview-Prep: added a sorting challenge

--- interview-prep/sorting.js
+++ interview-prep/sorting.js
@@ -90,6 +90,30 @@ function maximumToys(prices, max) {
 }
 
 /**
+ * C++ class used to rank player objects, each of which is defined by a name and a score property.
+ * Its only method is a comparator which ranks any two players as ascending, equal, or descending
+ * (1,0,-1 respectively), first by their score, then by their name (alphabetical order).
+ */
+class Checker{
+public:
+  	// complete this method
+    static int comparator(Player a, Player b)  {
+        if(a.score < b.score) return -1;
+        if(a.score > b.score) return 1;
+        int i;
+        for(i = 0; i < a.name.length(); i++) {
+            if(i == b.name.length()) return -1;
+            if(a.name[i] < b.name[i]) return 1;
+            if(a.name[i] > b.name[i]) return -1;
+        }
+
+        if(i < b.name.length() - 1) return 1;
+        return 0;
+    }
+};
+
+
+/**
  * Counts the number of swaps necessary to put an array of integers in ascending order,
  * where each swap must be of adjacent elements, where the low-indexed element has a higher
  * original value than the high-indexed element. Performs a merge sort and counts how many
