6b0dd0df45b0c8041c66bcdcbc47b90c0f92386e
johnhungerford <hungerfordjustice@gmail.com>
2019-03-04 11:15:34 -0500

    Interview-Prep: added a sorting challenge

--- interview-prep/sorting.js
+++ interview-prep/sorting.js
@@ -28,6 +28,68 @@ function countSwaps(a) {
 }
 
 /**
+ * Calculates the number of items one could purchase with a given budget from an array
+ * of items of different prices. Uses a merge sort to put the list in ascending order,
+ * and adds up prices until the budget is reached.
+ * @param  {array}   prices  Array of prices, each corresponding to a single item
+ * @param  {integer} max     budget for purchasing: maximum amount to spend on 'prices'
+ * @return {intever}         Maximum number of elements that can be purchased from 'prices'
+ */
+function maximumToys(prices, max) {
+    function mergeSort(arrin) {
+        if (arrin.length === 1) return arrin;
+        if (arrin.length === 2) {
+            if (arrin[0] > arrin[1]) {
+                const x = arrin[0];
+                arrin[0] = arrin[1];
+                arrin[1] = x;
+            }
+
+            return arrin;
+        }
+
+        const mid = Math.ceil((arrin.length - 1) / 2);
+        var lo = [];
+        var hi = [];
+        for (let i = 0; i < mid; i++) {
+            lo.push(arrin[i]);
+            hi.push(arrin[i + mid]);
+        }
+
+        if (2 * mid < arrin.length) hi.push(arrin[arrin.length - 1]);
+
+        lo = mergeSort(lo);
+        hi = mergeSort(hi);
+
+        let iHi = 0;
+        let iLo = 0;
+        for (let i = 0; i < arrin.length; i++) {
+            if (iHi >= hi.length || lo[iLo] < hi[iHi]) {
+                arrin[i] = lo[iLo];
+                iLo += 1;
+            } else {
+                arrin[i] = hi[iHi];
+                iHi += 1;
+            }
+        }
+
+        return arrin;
+    }
+
+    prices = mergeSort(prices);
+
+    let spent = 0;
+    let n = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (spent + prices[i] > max) break;
+        spent += prices[i];
+        n += 1;
+    }
+
+    return n;
+}
+
+/**
  * Counts the number of swaps necessary to put an array of integers in ascending order,
  * where each swap must be of adjacent elements, where the low-indexed element has a higher
  * original value than the high-indexed element. Performs a merge sort and counts how many
