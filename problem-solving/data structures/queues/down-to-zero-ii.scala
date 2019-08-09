f17399938017532480e6ed962f733cc5f131ea39
johnhungerford <hungerfordjustice@gmail.com>
2019-08-09 12:24:44 -0400

    added my first scala solution

---
+++ problem-solving/data structures/queues/down-to-zero-ii.scala
@@ -0,0 +1,25 @@
+var minArr: Array[Int] = Array.fill(1000001) { -1 }
+minArr(0) = 0; minArr(1) = 1; minArr(2) = 2
+
+var max = 2
+
+def downToZero(n: Int): Int = {
+    /*
+        * Write your code here.
+        */
+    while (max < n) {
+        // if (max > 500000) print(s"${max}, ${n}; ")
+        val nextMin = minArr(max) + 1
+        if (minArr(max + 1) == -1 || minArr(max + 1) > nextMin) 
+            minArr(max + 1) = nextMin
+
+        (2 to math.min(max, 1000000 / max)).foreach((x) => {
+            if (minArr(x * max) == -1 || minArr(x * max) > nextMin)
+                minArr(x * max) = nextMin
+        })
+        
+        max += 1
+    }
+
+    return minArr(n)
+}
